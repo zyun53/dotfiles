74febdd02633bf728d761398f03c54c4b1dd3656
Isida Zyun'iti <zyun@moin.jp>
2021-01-13 23:21:34 +0900

    Fix installer

--- install.sh
+++ install.sh
@@ -1,11 +1,23 @@
-mkdir -p ~/.config/nvim/backup ~/.config/nvim/swap ~/.config/nvim/undo
+#!/bin/bash
 
-ln -fs ~/dotfiles/zshrc ~/.zshrc
-ln -fs ~/dotfiles/tmux.conf ~/.tmux.conf
-ln -fs ~/dotfiles/gitconfig ~/.gitconfig
-ln -fs ~/dotfiles/aliases ~/.aliases
-ln -fs ~/dotfiles/my.cnf ~/.my.cnf
-ln -fs ~/dotfiles/grcat ~/.grcat
+DOTFILES_PATH=~/src/github.com/zyun-i/dotfiles
 
-ln -fs ~/dotfiles/vimrc ~/.vimrc
-ln -fs ~/.config/nvim ~/.vim
+while read line
+do
+    ln -fs ${DOTFILES_PATH}/${line} ~/.${line}
+done << EOF
+tmux.conf
+gitconfig
+zshrc
+vimrc
+EOF
+
+# mkdir -p ~/.config/nvim/backup ~/.config/nvim/swap ~/.config/nvim/undo
+# ln -fs ~/dotfiles/zshrc ~/.zshrc
+# ln -fs ~/dotfiles/tmux.conf ~/.tmux.conf
+# ln -fs ~/dotfiles/gitconfig ~/.gitconfig
+# ln -fs ~/dotfiles/aliases ~/.aliases
+# ln -fs ~/dotfiles/my.cnf ~/.my.cnf
+# ln -fs ~/dotfiles/grcat ~/.grcat
+# ln -fs ~/dotfiles/vimrc ~/.vimrc
+# ln -fs ~/.config/nvim ~/.vim
