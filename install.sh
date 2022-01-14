acdc3a3ef04fe8e1821f41f2d791774b2e00264e
Isida Zyun'iti <zyun@moin.jp>
2022-01-14 14:36:51 +0900

    Change install script.

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash -x
 
-# mkdir -p ~/.ansible/vault
-
 DOTFILES_PATH=~/src/github.com/zyun-i/dotfiles
 
 while read line
@@ -15,15 +13,7 @@ vimrc
 EOF
 
 ln -fs ${DOTFILES_PATH}/gpg.conf ~/.gnupg/gpg.conf
+ln -fs ${DOTFILES_PATH}/starship.toml ~/.config/starship.toml
 
 mkdir -p ~/.vim/backup ~/.vim/swap
-
-# mkdir -p ~/.config/nvim/backup ~/.config/nvim/swap ~/.config/nvim/undo
-# ln -fs ~/dotfiles/zshrc ~/.zshrc
-# ln -fs ~/dotfiles/tmux.conf ~/.tmux.conf
-# ln -fs ~/dotfiles/gitconfig ~/.gitconfig
-# ln -fs ~/dotfiles/aliases ~/.aliases
-# ln -fs ~/dotfiles/my.cnf ~/.my.cnf
-# ln -fs ~/dotfiles/grcat ~/.grcat
-# ln -fs ~/dotfiles/vimrc ~/.vimrc
-# ln -fs ~/.config/nvim ~/.vim
+mkdir -p ~/.config/nvim/backup ~/.config/nvim/swap ~/.config/nvim/undo
