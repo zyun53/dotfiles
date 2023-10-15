834ccc983d9be1434a1fccd81d7b117deef51e3b
Isida Zyun'iti <mail@zyun.jp>
2023-10-15 22:33:42 +0900

    Fix option vim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,184 +1,24 @@
-vim.g.mapleader = [[ ]]
-vim.g.maplocalleader = [[,]]
-
-require "plugins"
-
-vim.cmd("colorscheme nightfox")
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ','
 
 require 'options'
 
-local cmp = require'cmp'
-cmp.setup({
-    snippet = {
-        -- REQUIRED - you must specify a snippet engine
-        expand = function(args)
-            vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-            -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-            -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-            -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-        end,
-    },
-    window = {
-        -- completion = cmp.config.window.bordered(),
-        -- documentation = cmp.config.window.bordered(),
-    },
-    mapping = cmp.mapping.preset.insert({
-        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.abort(),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-    }),
-    sources = cmp.config.sources({
-        { name = 'nvim_lsp' },
-        { name = 'vsnip' }, -- For vsnip users.
-        -- { name = 'luasnip' }, -- For luasnip users.
-        -- { name = 'ultisnips' }, -- For ultisnips users.
-        -- { name = 'snippy' }, -- For snippy users.
-    }, {
-        { name = 'buffer' },
-    })
-})
+vim.cmd("colorscheme nightfox")
 
--- Set configuration for specific filetype.
-cmp.setup.filetype('gitcommit', {
-    sources = cmp.config.sources({
-        { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
-    }, {
-        { name = 'buffer' },
-    })
-})
+-- dpp.vim
+local cache = vim.fn.expand('~/.cache/dpp/')
 
--- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline({ '/', '?' }, {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = {
-        { name = 'buffer' }
-    }
-})
+if vim.fn.isdirectory(cache) == 0 then
+  vim.fn.mkdir(cache, 'p')
+end
 
--- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline(':', {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = cmp.config.sources({
-        { name = 'path' }
-    }, {
-        { name = 'cmdline' }
-    })
-})
+local dpp_src = cache .. "repos/github.com/Shougo/dpp.vim"
+local denops_src = '~/.cache/dpp/repos/github.com/denops/denops.vim'
 
--- Set up lspconfig.
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
+vim.opt.rtp:prepend(dpp_src)
 
-require("mason").setup()
-require("mason-lspconfig").setup()
-
-local lsp_flags = {
-  -- This is the default in Nvim 0.7+
-  debounce_text_changes = 150,
-}
-require('lspconfig')['pyright'].setup{
-    on_attach = on_attach,
-    flags = lsp_flags,
-}
-require('lspconfig')['tsserver'].setup{
-    on_attach = on_attach,
-    flags = lsp_flags,
-}
-require('lspconfig')['rust_analyzer'].setup{
-    on_attach = on_attach,
-    flags = lsp_flags,
-    -- Server-specific settings...
-    settings = {
-      ["rust-analyzer"] = {}
-    }
-}
-require('lspconfig')['marksman'].setup{
-    on_attach = on_attach,
-    flags = lsp_flags,
-}
-require('lspconfig')['gopls'].setup{
-    on_attach = on_attach,
-    flags = lsp_flags,
-}
-
--- null-ls
-local null_ls = require("null-ls")
-
-null_ls.setup({
-    sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.diagnostics.eslint,
-        null_ls.builtins.completion.spell,
-        null_ls.builtins.diagnostics.markdownlint,
-        null_ls.builtins.diagnostics.textlint.with({ filetypes = { "markdown" } }),
-    },
-})
-
-
--- telescope
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-
--- visual
-require('lualine').setup()
-
--- git
-require('git').setup({
-  default_mappings = true, -- NOTE: `quit_blame` and `blame_commit` are still merged to the keymaps even if `default_mappings = false`
-
-  keymaps = {
-    -- Open blame window
-    blame = "<Leader>gb",
-    -- Close blame window
-    quit_blame = "q",
-    -- Open blame commit
-    blame_commit = "<CR>",
-    -- Open file/folder in git repository
-    browse = "<Leader>go",
-    -- Open pull request of the current branch
-    open_pull_request = "<Leader>gp",
-    -- Create a pull request with the target branch is set in the `target_branch` option
-    create_pull_request = "<Leader>gn",
-    -- Opens a new diff that compares against the current index
-    diff = "<Leader>gd",
-    -- Close git diff
-    diff_close = "<Leader>gD",
-    -- Revert to the specific commit
-    revert = "<Leader>gr",
-    -- Revert the current file to the specific commit
-    revert_file = "<Leader>gR",
-  },
-  -- Default target branch when create a pull request
-  target_branch = "main",
-})
-
-require'nvim-treesitter.configs'.setup {
-  highlight = {
-    enable = true,
-    disable = {},
-  },
-  indent = {
-    enable = true,
-    disable = {},
-  },
-  ensure_installed = {
-    "tsx",
-    "toml",
-    "fish",
-    "php",
-    "json",
-    "yaml",
-    "swift",
-    "css",
-    "html",
-    "lua",
-    "astro",
-  },
-  autotag = {
-    enable = true,
-  },
-}
+if vim.fn['dpp#min#load_state'] == 0 then
+  print('ok')
+  else
+  print('error')
+end
