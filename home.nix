513319ffacd443c78057533616ba1f6ba3135cd8
Isida Zyun'iti <mail@zyun.jp>
2024-04-03 22:47:44 +0900

    Fix alacritty git

--- home.nix
+++ home.nix
@@ -9,6 +9,8 @@
   home.packages = [
     pkgs.ripgrep
     pkgs.bat
+    pkgs.fd
+    pkgs.htop
   ];
 
   home.file = {};
@@ -29,6 +31,121 @@
       br = "branch";
       co = "checkout";
       st = "status";
+      dfc = "diff --cached";
+      lg = "log --oneline --graph --decorate";
+      rs = "reset";
+    };
+    extraConfig = {
+      core = {
+        editor = "nvim";
+      };
+      advice = {
+        skippedCherryPicks = false;
+      };
+      color = {
+        ui = "auto";
+      };
+      init = {
+        defaultBranch = "main";
+      };
+      merge = {
+        conflictStyle = "diff3";
+        ff = false;
+      };
+      pull = {
+        ff = "only";
+      };
+      push = {
+        default = "current";
+      };
+      rebase = {
+        autosquash = true;
+        autostash = true;
+        stat = true;
+      };
+      rerere = {
+        enabled = true;
+      };
+    };
+    ignores = [
+      "*~"
+      "*.swp"
+      ".DS_Store"
+      ".direnv"
+    ];
+    #signing = {
+    #  key = "";
+    #};
+  };
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      colors = {
+        primary = {
+          background = "#192330";
+          foreground = "#cdcecf";
+        };
+        normal = {
+          black   = "#393b44";
+          red     = "#c94f6d";
+          green   = "#81b29a";
+          yellow  = "#dbc074";
+          blue    = "#719cd6";
+          magenta = "#9d79d6";
+          cyan    = "#63cdcf";
+          white   = "#dfdfe0";
+        };
+        bright = {
+          black   = "#575860";
+          red     = "#d16983";
+          green   = "#8ebaa4";
+          yellow  = "#e0c989";
+          blue    = "#86abdc";
+          magenta = "#baa1e2";
+          cyan    = "#7ad5d6";
+          white   = "#e4e4e5";
+        };
+      };
+      cursor = {
+        style = {
+          blinking = "On";
+        };
+      };
+      font = {
+        size = 16;
+        normal = {
+          family = "PlemolJP Console NF";
+          style = "Regular";
+        };
+        bold = {
+          style = "Bold";
+        };
+        italic = {
+          style = "Italic";
+        };
+        bold_italic = {
+          style = "Bold Italic";
+        };
+      };
+      window = {
+        padding = {
+          x = 4;
+          y = 4;
+        };
+        dimensions = {
+          columns = 100;
+          lines = 40;
+        };
+      };
+      scrolling = {
+        history = 10000;
+        multiplier = 3;
+      };
+      keyboard = {
+        bindings = [
+          { key = "Enter"; action = "ToggleFullscreen"; mods = "Command"; }
+        ];
+      };
     };
   };
 }
